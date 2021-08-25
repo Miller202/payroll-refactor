55204bee915c3a49c4c8e46289b96f5347df897e
Michael Miller <mullerg4michael@gmail.com>
2021-08-25 15:47:27 -0300

    First commit

---
+++ src/payroll/main/GeneralUtils.java
@@ -0,0 +1,84 @@
+package payroll.main;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class GeneralUtils {
+
+    public static LocalDate readData(Scanner input){
+        System.out.println("Digite o dia (número):");
+        int day = input.nextInt();
+        System.out.println("Digite o mês (número):");
+        int month = input.nextInt();
+        System.out.println("Digite o ano:");
+        int year = input.nextInt();
+        input.nextLine();
+
+        return LocalDate.of(year, month, day);
+    }
+
+    public static LocalTime readTime(Scanner input){
+        System.out.println("Digite a hora:");
+        int hour = input.nextInt();
+        System.out.println("Digite o minuto:");
+        int minute= input.nextInt();
+
+        return LocalTime.of(hour, minute);
+    }
+
+    public static LocalDate getLastJobDay(LocalDate lastDayOfMonth){
+        LocalDate lastJobDay;
+
+        if(lastDayOfMonth.getDayOfWeek() == DayOfWeek.SATURDAY){
+            lastJobDay = lastDayOfMonth.minusDays(1);
+        }else if(lastDayOfMonth.getDayOfWeek() == DayOfWeek.SUNDAY){
+            lastJobDay = lastDayOfMonth.minusDays(2);
+        }else{
+            lastJobDay = lastDayOfMonth;
+        }
+
+        return lastJobDay;
+    }
+
+    public static String readPayMethod(Scanner input){
+        System.out.println("Qual o método de pagamento?");
+        System.out.println("[1] - Cheque nos correios, [2] - Depósito bancário, [3] - Em mãos\n");
+        int methodChoice = input.nextInt();
+
+        String payMethod;
+        if(methodChoice == 1){
+            payMethod = "Cheque nos correios";
+        }else if(methodChoice == 2){
+            payMethod = "Depósito bancário";
+        }else if(methodChoice == 3){
+            payMethod = "Em mãos";
+        }else{
+            payMethod = "Depósito bancário";
+        }
+
+        return payMethod;
+    }
+
+    // no longer used but may be reused soon
+    public static String readSchedule(Scanner input){
+        System.out.println("Como deseja receber o seu salário?");
+        System.out.println("[1] - Mensal, [2] - Semanal, [3] - Bisemanal\n");
+        int scheduleChoice = input.nextInt();
+
+        String schedule;
+        if(scheduleChoice == 1){
+            schedule = "Mensal";
+        }else if(scheduleChoice == 2){
+            schedule = "Semanal";
+        }else if(scheduleChoice == 3){
+            schedule = "Bisemanal";
+        }else{
+            schedule = "Mensal";
+        }
+
+        return schedule;
+    }
+
+}
